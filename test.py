8124ea7d38c308870f14dffdbaa9a56276505c84
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2017-09-17 18:10:43 +0000

    python calculator

---
+++ test.py
@@ -0,0 +1,10 @@
+def add(x, y):
+   return x + y
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+choice = input("Enter choice(1/2/3/4):")
+
+#if choice == '1':
+#print(num1,"+",num2,"=", add(num1,num2))
+if choice == 1:
+   print (num1,"+",num2,"=", add(num1,num2))
