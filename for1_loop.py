23aa6a68c407eeed5007f818b9f88e9b5efa795c
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2017-09-18 16:51:10 +0000

    for

---
+++ for1_loop.py
@@ -0,0 +1,15 @@
+# Program to iterate through a list using indexing
+
+genre = ['pop', 'rock', 'jazz']
+
+# iterate over the list using index
+#for i in range(len(genre)):
+for i in genre:
+	print ("I like", i)
+
+#digits = [0, 1, 5]
+
+#for i in digits:
+#    print(i)
+#else:
+#    print("No items left.")
---
+++ for_loop.py
@@ -0,0 +1,7 @@
+num = [1,7,3,6,4,9,6,8,10]
+sum = 0
+for val in num:
+	sum = sum+val
+
+# Output: The sum is 48
+print("The sum is", sum)
