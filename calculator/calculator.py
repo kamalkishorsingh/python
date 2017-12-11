af3d7d610d042cac2d331314d89402ca809a07b1
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2017-12-11 04:55:10 +0000

    instance snapshot creation

---
+++ calculator/calculator.py
@@ -0,0 +1,21 @@
+#calculator that can add, subtract, multiply and divide
+from __future__ import division
+# This program adds two numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+#num1 = num11
+#num2 = num12
+#sum = float(num1) + num2
+sum = num1 + num2
+print("Sum of the given two number:",sum)
+#print ('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+sub = num1 - num2
+print("Sub of the given two number:",sub)
+
+mul = num1 * num2
+print("mul of the given two number:",mul)
+
+dev = num1 / num2
+print("dev of the given two number:",dev)
