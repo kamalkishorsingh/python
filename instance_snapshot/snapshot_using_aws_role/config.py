96955bc0c1cd6badf2961a4a587eb396a593fd6a
opstree <saurabh.vajpayee@opstree.com>
2018-02-16 15:31:06 +0530

    create aws snapshout without key using AWS role

---
+++ instance_snapshot/snapshot_using_aws_role/config.py
@@ -0,0 +1,10 @@
+config = {
+
+    'ec2_region_name': 'us-east-1',
+    'ec2_region_endpoint': 'ec2.us-east-1.amazonaws.com',
+    'owner' : '356254565629',
+    'prod_vol_id' : 'vol-0a057e1a24f7c6c5a',
+    'prod_days_old' : '2',
+    'tag_name': 'Name',
+    'tag_value': 'prod_mongo_snapshot-',
+}
